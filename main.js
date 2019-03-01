503bafcc30f3ed2e973f3c5cbcdc8f2e780c1c87
Tobias Beese <tobias.beese@gmx.de>
2019-03-01 09:31:59 +0100

    Move JS into separate file

---
+++ main.js
@@ -0,0 +1,23 @@
+let lastTimestamp = new Date()
+
+requestAnimationFrame(clock)
+
+function clock(timestamp) {
+  let now = new Date()
+  let delta = now - lastTimestamp
+
+  if (delta > 1 * 1000) {
+    lastTimestamp = now
+    let timeOffset = now.getTimezoneOffset() * 60
+    let dateValue = now.valueOf() / 1000
+
+    let epoch = Math.floor(dateValue)
+    let excelDate = ((dateValue - timeOffset) / 86400) + 25569
+
+    document.getElementById('clock').innerText = epoch
+    document.getElementById('excel').innerHTML = excelDate.toLocaleString('de-DE', { minimumFractionDigits: 5, maximumFractionDigits: 5 })
+    document.getElementById('human').innerHTML = now.toLocaleString('de-DE')
+  }
+
+  requestAnimationFrame(clock)
+}
