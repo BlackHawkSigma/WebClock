3dc5d9ca1d4c6857e27d6be8fb50d6c5f06dd44c
Tobias Beese <tobias.beese@gmx.de>
2019-03-04 12:26:00 +0100

    Add basic PWA Features

--- main.js
+++ main.js
@@ -21,3 +21,7 @@ function clock(timestamp) {
 
   requestAnimationFrame(clock)
 }
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js')
+}
---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'static-cache-v1'
+const URLS_TO_CACHE = [
+  '/',
+  'index.html',
+  'main.css',
+  'main.js',
+  'https://fonts.googleapis.com/css?family=Ubuntu'
+]
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(cache => {
+      return cache.addAll(URLS_TO_CACHE)
+    })
+  )
+})
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+    .then(respose => {
+      if (respose) { return respose }
+
+      return fetch(event.request)
+    })
+  )
+})
